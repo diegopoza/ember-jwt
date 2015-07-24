d0401692774ef0a3a6baa5cdb25e86b66e39deb2
Diego Poza <diego.poza@southworks.net>
2015-07-24 18:49:24 -0300

    Fixed indentation

--- ember2app/app/controllers/login.js
+++ ember2app/app/controllers/login.js
@@ -1,24 +1,26 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  loginFailed: false,
-  isProcessing: false,
-  actions: {
-    login: function() {
-     this.setProperties({
-      loginFailed: false,
-      isProcessing: true
-     });
-     Ember.$.post("http://localhost:3001/sessions/create", { username: this.get("username"), password: this.get("password")})
-     .then(function(data) {
-      this.set("isProcessing", false);
-      var jwt = data.id_token;
-      localStorage.setItem('jwt', jwt);
-      this.target.transitionTo('index');
-     }.bind(this), function() {
-      this.set("isProcessing", false);
-      this.set("loginFailed", true);
-     }.bind(this));
+    loginFailed: false,
+    isProcessing: false,
+    actions: {
+        login: function() {
+            this.setProperties({
+                loginFailed: false,
+                isProcessing: true
+            });
+            Ember.$.post("http://localhost:3001/sessions/create", {
+                    username: this.get("username"),
+                    password: this.get("password")
+                })
+                .then(function(data) {
+                    this.set("isProcessing", false);
+                    var jwt = data.id_token;
+                    localStorage.setItem('jwt', jwt);
+                    this.target.transitionTo('index');
+                }.bind(this), function() {
+                    this.set("isProcessing", false);
+                }.bind(this));
+        }
     }
-  }
 });
